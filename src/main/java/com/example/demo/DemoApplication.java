7fa50735156850caef00a2fa491660d99e6d36d5
David Mark Meszaros <david.mark.meszaros@ibm.com>
2022-07-03 17:47:01 +0200

    Docker image build and deployment files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		for(String arg : args) {
+			System.out.println(arg);
+		}
+
+		if(args.length > 0 && args[0].equals("run")) {
+			System.out.println("CLI run");
+		} else {
+			System.out.println("REST run");
+			SpringApplication.run(DemoApplication.class, args);
+		}
 	}
 
 }
